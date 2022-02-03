536ffa0d874edf19976d188e4a89bfc541e6e32a
TjoAlex <alle.98@live.se>
2022-02-03 16:19:02 +0000

    Customized admin

--- products/models.py
+++ products/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -12,7 +15,7 @@ class Category(models.Model):
         return self.friendly_name
 
 class Product(models.Model):
-    Category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=245, null=True, blank=True)
     name = models.CharField(max_length=245)
     description = models.TextField()
@@ -24,3 +27,4 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+ 
