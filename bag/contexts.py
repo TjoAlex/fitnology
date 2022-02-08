4d2f98a919476ed5305209c1adcd9c5838ceb751
TjoAlex <alle.98@live.se>
2022-02-08 12:28:57 +0000

    Added add to bag functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,4 +1,4 @@
-from decimal importDecimal
+from decimal import Decimal
 from django.conf import settings
 
 def bag_contents(request):
@@ -8,21 +8,21 @@ def bag_contents(request):
     product_count = 0
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-
-        grand_total = delivery + total
-
+    
+    grand_total = delivery + total
+    
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
-        'free_delivery_treshold': free_delivery_treshold,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
 
