01cc914786ab4879dd98cb57254f25228ae5da28
TjoAlex <alle.98@live.se>
2022-02-15 22:02:47 +0000

    Added checkout form data chaching in payment intent

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,9 +1,8 @@
 /*
-Core logic/payment for this comes from:
-https://stripe.com/docs/stripe-js/appearance-api
-
-Css from here:
-https//stripe.com/docs/stripe-js
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
 */
 
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
@@ -13,11 +12,11 @@ var elements = stripe.elements();
 var style = {
   base: {
     color: '#000',
-    fontFamily: '"Dosis", sans-serif',
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
     fontSmoothing: 'antialiased',
     fontSize: '16px',
     '::placeholder': {
-      color: '#0c343d'
+      color: '#aab7c4'
     }
   },
   invalid: {
@@ -35,11 +34,11 @@ card.addEventListener('change', function (event) {
   var errorDiv = document.getElementById('card-errors');
   if (event.error) {
     var html = `
-          <span class="icon" role="alert">
-              <i class="fas fa-times"></i>
-          </span>
-          <span>${event.error.message}</span>
-      `;
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
     $(errorDiv).html(html);
   } else {
     errorDiv.textContent = '';
@@ -55,29 +54,71 @@ form.addEventListener('submit', function (ev) {
     'disabled': true
   });
   $('#submit-button').attr('disabled', true);
-  stripe.confirmCardPayment(clientSecret, {
-    payment_method: {
-      card: card,
-    }
-  }).then(function (result) {
-    if (result.error) {
-      // Show error to customer (e.g., insufficient funds)
-      var errorDiv = document.getElementById('card-errors');
-      var html = `
-                <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>`;
-      $(errorDiv).html(html);
-      card.update({
-        'disabled': false
-      });
-      $('#submit-button').attr('disabled', false);
-    } else {
-      // The payment has been processed!
-      if (result.paymentIntent.status === 'succeeded') {
-        form.submit();
+  $('#payment-form').fadeToggle(100);
+  $('#loading-overlay').fadeToggle(100);
+
+  var saveInfo = Boolean($('#id-save-info').attr('checked'));
+  // From using {% csrf_token %} in the form
+  var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+  var postData = {
+    'csrfmiddlewaretoken': csrfToken,
+    'client_secret': clientSecret,
+    'save_info': saveInfo,
+  };
+  var url = '/checkout/cache_checkout_data/';
+
+  $.post(url, postData).done(function () {
+    stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: card,
+        billing_details: {
+          name: $.trim(form.full_name.value),
+          phone: $.trim(form.phone_number.value),
+          email: $.trim(form.email.value),
+          address: {
+            line1: $.trim(form.street_address1.value),
+            line2: $.trim(form.street_address2.value),
+            city: $.trim(form.town_or_city.value),
+            country: $.trim(form.country.value),
+            state: $.trim(form.county.value),
+          }
+        }
+      },
+      shipping: {
+        name: $.trim(form.full_name.value),
+        phone: $.trim(form.phone_number.value),
+        address: {
+          line1: $.trim(form.street_address1.value),
+          line2: $.trim(form.street_address2.value),
+          city: $.trim(form.town_or_city.value),
+          country: $.trim(form.country.value),
+          postal_code: $.trim(form.postcode.value),
+          state: $.trim(form.county.value),
+        }
+      },
+    }).then(function (result) {
+      if (result.error) {
+        var errorDiv = document.getElementById('card-errors');
+        var html = `
+                    <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+        $(errorDiv).html(html);
+        $('#payment-form').fadeToggle(100);
+        $('#loading-overlay').fadeToggle(100);
+        card.update({
+          'disabled': false
+        });
+        $('#submit-button').attr('disabled', false);
+      } else {
+        if (result.paymentIntent.status === 'succeeded') {
+          form.submit();
+        }
       }
-    }
-  });
+    });
+  }).fail(function () {
+    // just reload the page, the error will be in django messages
+    location.reload();
+  })
 });
