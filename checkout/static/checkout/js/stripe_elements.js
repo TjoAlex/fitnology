fb23f3ddb8f9d2f9fc11ca3301b2eb28e7fbad2a
TjoAlex <alle.98@live.se>
2022-02-15 23:30:58 +0000

    Complete webhook handler

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -5,6 +5,13 @@
     https://stripe.com/docs/stripe-js
 */
 
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
@@ -12,11 +19,11 @@ var elements = stripe.elements();
 var style = {
   base: {
     color: '#000',
-    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontFamily: '"Dosis", sans-serif',
     fontSmoothing: 'antialiased',
     fontSize: '16px',
     '::placeholder': {
-      color: '#aab7c4'
+      color: '#0c343d'
     }
   },
   invalid: {
