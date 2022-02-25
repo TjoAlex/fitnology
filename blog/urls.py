e02c6268513f852902b098b3810da572058cfbd6
TjoAlex <alle.98@live.se>
2022-02-25 19:09:13 +0000

    Remove commented out code

--- blog/urls.py
+++ blog/urls.py
@@ -4,11 +4,10 @@ from . import views
 
 
 urlpatterns = [
-    path('blog/', views.PostList.as_view(), name='blog'),
+    path('', views.PostList.as_view(), name='blog'),
     path('addpost/', views.add_post, name='addpost'),
     path('<int:post_id>/', views.PostDetail, name='postdetail'),
     path('editpost/<int:post_id>/', views.edit_post, name='editpost'),
-    # path('deletepost/<int:id>/', views.delete_post, name='deletepost'),
     path('deletepost/<int:post_id>/', views.delete_post, name='deletepost'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 
