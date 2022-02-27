b696e2353f20dd545cc2b5ad943f988b0e6c31d4
TjoAlex <alle.98@live.se>
2022-02-27 11:18:56 +0000

    Removed secret key and set debug

--- fitnology/settings.py
+++ fitnology/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mqw26i3x8bd+5ku4e()dnwk^a(mgf+su2nl4_zs+=vbk18tora'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fitnology.herokuapp.com', 'localhost']
 
